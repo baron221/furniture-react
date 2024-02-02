ff757a1877d12b29ec0c7af214ec39271142e51d
Baron221 <baronjon080@gmail.com>
2024-02-02 16:37:02 +0900

    feat:integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../css/App.css';
+import { Box, Container, Stack, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{my:4}}>
+          <Typography variant='h2' component={"h3"}>
+            It is my furniture Reactapp with redux
+          </Typography>
+        </Box>
+      </Stack> 
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import './css/index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import "./css/index.css";
 
-const container = document.getElementById('root')!;
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
