d9d36b482dde8f79625765b6d341ed1fcd157f1a
Baron221 <baronjon080@gmail.com>
2024-02-04 17:37:21 +0900

    feat:developing footer down

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,11 +1,4 @@
-import {
-  Badge,
-  Box,
-  Container,
-  IconButton,
-  Stack,
-  TextField,
-} from "@mui/material";
+import { Box, Container, Stack, TextField } from "@mui/material";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -19,22 +12,40 @@ export function Footer() {
             Sign up for deals, new products and promotions
           </Box>
         </Stack>
-        <Stack display={"flex"} flexDirection={"row"} justifyContent={'center'} marginTop={'50px'}>
-          <Box marginRight={'15px'}>
+        <Stack
+          display={"flex"}
+          flexDirection={"row"}
+          justifyContent={"center"}
+          marginTop={"50px"}
+        >
+          <Box marginRight={"15px"}>
             <img
               width={"60px"}
               height={"60px"}
-              
               src="/iconsfurnis/iconemail.svg"
               alt="emailicon"
             />
           </Box>
-          <Box fontSize={'40px'} >
-            <TextField id="filled-basic" label="Email" variant="filled" />    
-              Signup
+          <Box fontSize={"40px"}>
+            <TextField id="filled-basic" label="Email" variant="filled" />
+            <button className="signup">SignUp</button>
           </Box>
         </Stack>
+      
       </Container>
+      
+      <Stack className="footer_down">
+          <Box>
+            <img
+              width={"105px"}
+              height={"24px"}
+              src="/imagesfurnis/logowhite.svg"
+              alt=""
+            />
+          </Box>
+          <Box className="line"></Box>
+        </Stack>
+      
     </div>
   );
 }
