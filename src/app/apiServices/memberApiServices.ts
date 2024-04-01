0b98137170f321200916a384515995d8b7254eb5
Baron221 <baronjon080@gmail.com>
2024-04-01 10:56:57 +0900

    develop signup  process

--- src/app/apiServices/memberApiServices.ts
+++ src/app/apiServices/memberApiServices.ts
@@ -16,13 +16,34 @@ class MemberApiService {
       });
       console.log("state:", (await result).data.state);
       assert.ok((await result)?.data, Definer.general_err2);
-      assert.ok((await result)?.data?.state != "fail", (await result)?.data?.message);
+      assert.ok(
+        (await result)?.data?.state != "fail",
+        (await result)?.data?.message
+      );
 
       const member: Member = (await result).data.data;
-      localStorage.setItem("member_data",JSON.stringify(member));
+      localStorage.setItem("member_data", JSON.stringify(member));
       return member;
     } catch (err: any) {
       console.log(`ERROR:::loginRequest${err.message}`);
+      throw err;
+    }
+  }
+
+  public async signupRequest(signup_data: any) {
+    try {
+      const result = await axios.post(this.path + "/signup", signup_data, {
+        withCredentials: true,
+      });
+      console.log("state", result.data.state);
+      assert.ok(result?.data, Definer.general_err2);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
+
+      const member: Member = result.data.data;
+      localStorage.setItem("member_data", JSON.stringify(member));
+      return member;
+    } catch (err: any) {
+      console.log(`ERROR:::signUpRequest ${err.message}`);
       throw err;
     }
   }
