a3a8f8b6266ec69120996de1a492ede8203d6e30
Baron221 <baronjon080@gmail.com>
2024-05-14 14:12:35 +0900

    feat:organize retrieve of memberPage query

--- src/app/screens/LoginPage/index.tsx
+++ src/app/screens/LoginPage/index.tsx
@@ -15,14 +15,15 @@ export function LoginPage(props: any) {
 const {verifiedMemberData}= props;
     let shop = useRouteMatch();
     const query = useQuery();
-
+const chosen_mb_id:string | null = query.get("mb_id") ?? null;
+const chosen_art_id:string | null = query.get("art_id") ??  null
 
 
     return (
         <div className="restaurant_page">
             <Switch>
                 <Route path={`${shop.path}/other`}>
-                    <VisitOtherPage verifiedMemberData={verifiedMemberData} />
+                    <VisitOtherPage verifiedMemberData={verifiedMemberData}  chosen_mb_id={chosen_mb_id} chosen_art_id={chosen_art_id}/>
 
                 </Route>
                 <Route path={`${shop.path}`}>
